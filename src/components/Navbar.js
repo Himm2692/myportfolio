1dc58d0a7a81af86a898750fcc311227532054f5
himanshu nigam <hnigam743@gmail.com>
2023-10-19 19:03:17 +0530

    update

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,21 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './home.css'
 import { NavLink } from 'react-router-dom'
 
 const Navbar = () => {
+  const [hamburger , setHamburger] = useState(false)
   return (
     <header>
-        <nav>
+        <nav className='navbar'>
             <div className='left-nav'>Himanshu's portfolio</div>
             <div className='right-nav'>
-                <ul>
+                <ul className={hamburger? " mobile-nav-link" : "nav-link"}>
                     <li><NavLink to="/">Home</NavLink></li>
                     <li><NavLink to="/about">About</NavLink></li>
                    <li> <NavLink to="/projects">Projects</NavLink></li>
                     <li><NavLink to="/contact">Contact</NavLink></li>
                     
                 </ul>
+                
             </div>
+            <div className='hamburger'>
+                <i class="fa-solid fa-bars" onClick={()=>setHamburger(!hamburger)}></i>
+                </div>
         </nav>
     </header>
   )
